9e19ed889c4090b0645dbdc9eec50df9812095ee
ciro23 <ciro.sorre23@gmail.com>
2023-06-08 21:34:07 +0200

    initial upload

---
+++ src/main/java/it/tino/javamovieapp/JavaMovieAppApplication.java
@@ -0,0 +1,12 @@
+package it.tino.javamovieapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaMovieAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaMovieAppApplication.class, args);
+    }
+}
---
+++ src/test/java/it/tino/javamovieapp/JavaMovieAppApplicationTests.java
@@ -0,0 +1,12 @@
+package it.tino.javamovieapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaMovieAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
