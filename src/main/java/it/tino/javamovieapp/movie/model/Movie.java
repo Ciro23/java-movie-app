b13921885e4317cecc9bac60a5985976c747aaa2
ciro23 <ciro.sorre23@gmail.com>
2023-06-08 22:20:42 +0200

    added movie entity

---
+++ src/main/java/it/tino/javamovieapp/movie/model/Movie.java
@@ -0,0 +1,30 @@
+package it.tino.javamovieapp.movie.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Movie {
+
+    private int id;
+    private String title;
+    private float voteAverage;
+    private String releaseDate;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Movie movie = (Movie) o;
+
+        return id == movie.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return id;
+    }
+}
