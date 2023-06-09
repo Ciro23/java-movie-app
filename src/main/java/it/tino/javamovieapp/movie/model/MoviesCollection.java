5481bebba062d88426b85cc33bad90ad9338543f
ciro23 <ciro.sorre23@gmail.com>
2023-06-09 20:56:36 +0200

    added movies collection entity

---
+++ src/main/java/it/tino/javamovieapp/movie/model/MoviesCollection.java
@@ -0,0 +1,24 @@
+package it.tino.javamovieapp.movie.model;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class MoviesCollection {
+
+    @JsonProperty("page")
+    int currentPage = 1;
+
+    @JsonProperty("total_pages")
+    int totalPages = 1;
+
+    @JsonProperty("results")
+    Collection<Movie> movies = new ArrayList<>();
+}
