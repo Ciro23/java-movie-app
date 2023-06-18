b0f0c0a2ecce9dc69f8f2de21921409bb73acaab
cirodigibite <ciro.sorre23@gmail.com>
2023-06-18 12:16:48 +0200

    login controller

---
+++ src/main/java/it/tino/javamovieapp/user/controller/LoginController.java
@@ -0,0 +1,13 @@
+package it.tino.javamovieapp.user.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LoginController {
+
+    @GetMapping("login")
+    public String showLoginPage() {
+        return "login";
+    }
+}
